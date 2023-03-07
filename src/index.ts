e22a4814cbe47b1b8951eafe6b04ec032ee2e872
Chiplinka <37448724+Chiplinka@users.noreply.github.com>
2023-03-07 11:45:09 +0300

    All variables (I hope) are explicitly defined

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,25 @@ const comicLink: string = "https://getxkcd.vercel.app/api/comic"
 
 
 async function getComciId(): Promise<number> {
-  const url = new URL(iuLink);
+  const urlGetId: URL = new URL(iuLink);
   const queryParams: URLSearchParams = new URLSearchParams({ email: myEmail })
-  url.search = new URLSearchParams(queryParams).toString();
+  urlGetId.search = new URLSearchParams(queryParams).toString();
 
-  const response: Response = await fetch(url)
+  const response: Response = await fetch(urlGetId)
   return await response.json()
 }
 
 async function renderComic() {
   const comicId: number = await getComciId();
 
-  const url = new URL(comicLink);
-  url.search = new URLSearchParams({ num: comicId.toString() }).toString();
+  const urlGetComic: URL = new URL(comicLink);
+  urlGetComic.search = new URLSearchParams({ num: comicId.toString() }).toString();
 
-  const comic: comicInterface = await fetch(url).then(response => response.json());
+  const comic: comicInterface = await fetch(urlGetComic).then(response => response.json());
 
-  const title = document.getElementById("titleComic") as HTMLHeadingElement;
-  const image = document.getElementById("imgComic") as HTMLImageElement;
-  const date = document.getElementById("dateComic") as HTMLParagraphElement;
+  const title: HTMLHeadingElement = document.getElementById("titleComic") as HTMLHeadingElement;
+  const image: HTMLImageElement = document.getElementById("imgComic") as HTMLImageElement;
+  const date: HTMLParagraphElement = document.getElementById("dateComic") as HTMLParagraphElement;
 
   const dateComic: Date = new Date(parseInt(comic.year), parseInt(comic.month), parseInt(comic.day));
   title.textContent = comic.title;
