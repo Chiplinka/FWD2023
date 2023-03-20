91c099d10bbbf3985cf597be15b9085741c4dfc8
Chiplinka <37448724+Chiplinka@users.noreply.github.com>
2023-03-20 22:50:57 +0300

    Removee comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-  import Header from "../components/Header.svelte";
   import Body from "../components/Body.svelte";
   import Skills from "../components/Skills.svelte";
-  // import Comic from "../components/Comic.svelte";
-  import Footer from "../components/Footer.svelte";
 </script>
 
 <svelte:head>
