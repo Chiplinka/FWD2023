46fd2b598f42fd74f6f6bad5b784e548ac88b7c4
Chiplinka <37448724+Chiplinka@users.noreply.github.com>
2023-03-22 23:50:30 +0300

    Remove unnecessary files

--- src/routes/comic/+page.svelte
+++ src/routes/comic/+page.svelte
@@ -1,32 +1,5 @@
 <script lang="ts">
-  import type { comicInterface } from "./Comic";
-  const myEmail: string = "s.pasynkov@innopolis.university";
-  const iuLink: string = "https://fwd.innopolis.app/api/hw2";
-  const comicLink: string = "https://getxkcd.vercel.app/api/comic";
-
-  async function getComciId(): Promise<number> {
-    const urlGetId: URL = new URL(iuLink);
-    const queryParams: URLSearchParams = new URLSearchParams({
-      email: myEmail,
-    });
-    urlGetId.search = new URLSearchParams(queryParams).toString();
-
-    const response: Response = await fetch(urlGetId);
-    return await response.json();
-  }
-
-  async function getComic() {
-    const comicId: number = await getComciId();
-
-    const urlGetComic: URL = new URL(comicLink);
-    urlGetComic.search = new URLSearchParams({
-      num: comicId.toString(),
-    }).toString();
-
-    const comic: Response = await fetch(urlGetComic);
-    return comic.json();
-  }
-  const comicImage: Promise<comicInterface> = getComic();
+  import Comic from "../../components/Comic.svelte";
 </script>
 
 <svelte:head>
@@ -34,19 +7,4 @@
   <meta name="description" content="Comic" />
 </svelte:head>
 
-<div>
-  {#await comicImage}
-    <p>Loading...</p>
-  {:then comicImage}
-    <h2>{comicImage.title}</h2>
-    <p>Date: {comicImage.day}.{comicImage.month}.{comicImage.year}</p>
-    <img src={comicImage.img} alt={comicImage.alt} title={comicImage.title} />
-  {/await}
-</div>
-
-<style>
-  img {
-    width: 100%;
-    max-width: 50%;
-  }
-</style>
+<Comic myEmail={"s.pasynkov@innopolis.university"} />
--- src/routes/comic/Comic.ts
+++
@@ -1,8 +0,0 @@
-export interface comicInterface {
-    day: string;
-    month: string;
-    year: string;
-    alt: string;
-    img: string;
-    title: string;
-}
